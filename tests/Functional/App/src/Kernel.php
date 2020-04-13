7ebecc4b630c9851cad31acae7692a90bfef837e
Matthieu Napoli <matthieu@mnapoli.fr>
2020-04-13 18:51:06 +0200

    Add basic functional tests

--- tests/Functional/App/src/Kernel.php
+++ tests/Functional/App/src/Kernel.php
@@ -2,14 +2,14 @@
 
 namespace App;
 
+use Bref\SymfonyBridge\BrefKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
-class Kernel extends BaseKernel
+class Kernel extends BrefKernel
 {
     use MicroKernelTrait;
 

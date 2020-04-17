8e3f1a358286300960883558b592b975a86fa520
Matthieu Napoli <matthieu@mnapoli.fr>
2020-04-17 15:52:46 +0200

    Update the bridge to avoid copying the cache directory
    The compiled container is used from the deployed cache directory (if it exists).

--- src/BrefKernel.php
+++ src/BrefKernel.php
@@ -2,7 +2,6 @@
 
 namespace Bref\SymfonyBridge;
 
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Kernel;
 
 abstract class BrefKernel extends Kernel
@@ -17,7 +16,10 @@ abstract class BrefKernel extends Kernel
      */
     public function getCacheDir()
     {
-        if ($this->isLambda()) {
+        // If we are running on Lambda and the `var/cache` directory does not exist,
+        // we move it to `/tmp` because it is a directory we can write to.
+        // That will allow us to compile the container there (which we can't do in `var/cache`).
+        if ($this->isLambda() && !is_dir(parent::getCacheDir())) {
             return '/tmp/cache/' . $this->environment;
         }
 
@@ -34,86 +36,5 @@ abstract class BrefKernel extends Kernel
         }
 
         return parent::getLogDir();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function boot()
-    {
-        // When on the lambda, copy the cache dir over to /tmp where it is writable
-        if ($this->isLambda() && ! is_dir($this->getCacheDir())) {
-            $this->prepareCacheDir(parent::getCacheDir(), $this->getCacheDir());
-        }
-
-        return parent::boot();
-    }
-
-    /**
-     * Return the pre-warmed directories in var/cache/[env] that should be copied to
-     * a writable directory in the Lambda environment.
-     *
-     * For optimal performance one should prewarm the cache folder and override this
-     * function to return an empty array.
-     */
-    protected function getWritableCacheDirectories(): array
-    {
-        return ['pools'];
-    }
-
-    protected function prepareCacheDir(string $readOnlyDir, string $writeDir): void
-    {
-        if (! is_dir($readOnlyDir)) {
-            return;
-        }
-
-        $startTime = microtime(true);
-        $cacheDirectoriesToCopy = $this->getWritableCacheDirectories();
-        $filesystem = new Filesystem;
-        $filesystem->mkdir($writeDir);
-
-        $scandir = scandir($readOnlyDir, SCANDIR_SORT_NONE);
-        if ($scandir === false) {
-            return;
-        }
-
-        foreach ($scandir as $item) {
-            if (in_array($item, ['.', '..'])) {
-                continue;
-            }
-
-            // Copy directories to a writable space on Lambda.
-            if (in_array($item, $cacheDirectoriesToCopy)) {
-                $filesystem->mirror("$readOnlyDir/$item", "$writeDir/$item");
-                continue;
-            }
-
-            // Symlink all other directories
-            // This is especially important with the Container* directories since it uses require_once statements
-            if (is_dir("$readOnlyDir/$item")) {
-                $filesystem->symlink("$readOnlyDir/$item", "$writeDir/$item");
-                continue;
-            }
-
-            // Copy all other files.
-            $filesystem->copy("$readOnlyDir/$item", "$writeDir/$item");
-        }
-
-        $this->logToStderr(sprintf(
-            'Symfony cache directory prepared in %s ms.',
-            number_format((microtime(true) - $startTime) * 1000, 2)
-        ));
-    }
-
-    /**
-     * This method logs to stderr.
-     *
-     * It must only be used in a lambda environment since all error output will be logged.
-     *
-     * @param string $message The message to log
-     */
-    protected function logToStderr(string $message): void
-    {
-        file_put_contents('php://stderr', date('[c] ') . $message . PHP_EOL, FILE_APPEND);
     }
 }

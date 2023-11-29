aaa3cc46779142af495e5ab21400df3927c53840
Gábor Egyed <gabor.egyed@gmail.com>
2023-11-30 00:59:15 +0100

    Add missing return types and attributes for tests

--- tests/Functional/App/src/Command/WriteToCacheCommand.php
+++ tests/Functional/App/src/Command/WriteToCacheCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -10,6 +11,7 @@ use Symfony\Contracts\Cache\CacheInterface;
 /**
  * This command tests writing to the system cache.
  */
+#[AsCommand('write-to-cache')]
 class WriteToCacheCommand extends Command
 {
     protected static $defaultName = 'write-to-cache';
